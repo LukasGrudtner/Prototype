3a836a92ea3bbffbae75a5f1acee0a41aa024278
LukasGrüdtner <lukasgrudtner@hotmail.com>
2017-08-05 21:43:39 -0300

    added animation screen 4

--- core/src/com/prototype/Prototype.java
+++ core/src/com/prototype/Prototype.java
@@ -38,6 +38,9 @@ public class Prototype implements ApplicationListener {
     float VIRTUAL_WIDTH = 0;
     float VIRTUAL_HEIGHT = 0;
 
+    Texture[] textures;
+    float variacao = 0;
+    float movimentacao = -200;
     // Teste
 //    DialogBox dialogBox;
 
@@ -53,17 +56,32 @@ public class Prototype implements ApplicationListener {
         seta = new Sprite(new Texture(Gdx.files.internal("seta.png")));
         dialogBox.setPosition(50, 150);
         text = new BitmapFont();
-        text.getData().setScale(4);
+        text.getData().setScale(3);
 
 //        dialogBox = new DialogBox("Oi, eu sou o Goku!", new Texture(Gdx.files.internal("dialogBox.png")), batch, 50, 150, 300, 100, 4)
 
         alturaDispositivo = VIRTUAL_HEIGHT = Gdx.graphics.getHeight();
         larguraDispositivo = VIRTUAL_WIDTH = Gdx.graphics.getWidth();
 
+        dialogBox.setSize(larguraDispositivo - 80, alturaDispositivo - 600);
+
         // Configuração da Cãmera
         camera = new OrthographicCamera();
         camera.position.set(VIRTUAL_WIDTH/2, VIRTUAL_HEIGHT/2, 0);
         viewport = new StretchViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, camera);
+
+        textures = new Texture[11];
+        textures[0] = new Texture("pomba1.png");
+        textures[1] = new Texture("pomba2.png");
+        textures[2] = new Texture("pomba3.png");
+        textures[3] = new Texture("pomba4.png");
+        textures[4] = new Texture("pomba5.png");
+        textures[5] = new Texture("pomba6.png");
+        textures[6] = new Texture("pomba7.png");
+        textures[7] = new Texture("pomba8.png");
+        textures[8] = new Texture("pomba9.png");
+        textures[9] = new Texture("pomba10.png");
+        textures[10] = new Texture("pomba11.png");
 	}
 
 	@Override
@@ -107,40 +125,50 @@ public class Prototype implements ApplicationListener {
 
     private void act1() {
         dialogBox.draw(batch);
-        layout.setText(text, "Finalmente chegou\no dia de ir ao dentista!", Color.BLACK, dialogBox.getWidth(), Align.center, true);
-        text.draw(batch, layout, dialogBox.getX()+(dialogBox.getWidth()-layout.width)/2-40, dialogBox.getY()+(dialogBox.getHeight()+layout.height)/2+80);
+        layout.setText(text, "Finalmente chegou o dia de ir ao dentista!", Color.BLACK, dialogBox.getWidth(), Align.center, true);
+        text.draw(batch, layout, dialogBox.getX()+(dialogBox.getWidth()-layout.width)/2 - 20, dialogBox.getY()+(dialogBox.getHeight()+layout.height)/2 + 50);
     }
 
     private void act2() {
         dialogBox.draw(batch);
-        text.getData().setScale(3);
+        text.getData().setScale(2);
         layout.setText(text, "E por isso este roteiro irá me ajudar a entender e aprender tudo que é preciso para tornar este dia muito especial!", Color.BLACK, dialogBox.getWidth()-50, Align.center, true);
-        text.draw(batch, layout, dialogBox.getX()+(dialogBox.getWidth()-layout.width)/2, dialogBox.getY()+(dialogBox.getHeight()+layout.height)/2+80);
+        text.draw(batch, layout, dialogBox.getX()+(dialogBox.getWidth()-layout.width)/2, dialogBox.getY()+(dialogBox.getHeight()+layout.height)/2+50);
     }
 
     private void act3() {
         dialogBox.draw(batch);
 
-        rectangle.set(500, 50, 200, 125);
+        rectangle.set(larguraDispositivo-250, 50, 200, 125);
 
         seta.setSize(200, 125);
-        seta.setPosition(500, 50);
+        seta.setPosition(larguraDispositivo-250, 50);
         seta.draw(batch);
 
-        text.getData().setScale(3);
+        text.getData().setScale(2);
         layout.setText(text, "Quando você estiver pronto, aperte na seta para iniciarmos nossa jornada!", Color.BLACK, dialogBox.getWidth()-50, Align.center, true);
-        text.draw(batch, layout, dialogBox.getX()+(dialogBox.getWidth()-layout.width)/2, dialogBox.getY()+(dialogBox.getHeight()+layout.height)/2+80);
+        text.draw(batch, layout, dialogBox.getX()+(dialogBox.getWidth()-layout.width)/2, dialogBox.getY()+(dialogBox.getHeight()+layout.height)/2+50);
 
         if (Gdx.input.isTouched()) {
             int x = Gdx.input.getX();
             int y = Gdx.input.getY();
-            
+
             if (rectangle.contains(x, alturaDispositivo-y))
                 act = 4;
         }
     }
 
     private void act4() {
+        if (variacao > 10)
+            variacao = 0;
+
+        if (movimentacao > Gdx.graphics.getWidth())
+            movimentacao = -200;
+
+        batch.draw(textures[(int) variacao], movimentacao, 800);
+        variacao += Gdx.graphics.getDeltaTime() * 10;
+        movimentacao += Gdx.graphics.getDeltaTime() * 400;
+
         dialogBox.draw(batch);
         text.getData().setScale(5);
         layout.setText(text, "Tela 4", Color.BLACK, dialogBox.getWidth()-50, Align.center, true);
